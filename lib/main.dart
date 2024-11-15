3e67ced701741cd435ed6327fa62ccbaa3eab103
akabanov <alexander.kabanoff@gmail.com>
2024-11-15 20:55:51 +1300

    'add friend' and 'delete friend' implemented
    Convert MainApp from StatelessWidget to StatefulWidget to manage state and handle dynamic updates. Added functionality to remove friends from the list via a swipe gesture and to add new friends using a TextField input and FloatingActionButton.

--- lib/main.dart
+++ lib/main.dart
@@ -4,5 +4,5 @@ import 'package:flutter_study_skeleton_app/src/person/person.dart';
 import 'src/app.dart';
 
 void main() {
-  runApp(MainApp(friends: List.generate(100, (_) => Person.random())));
+  runApp(MainApp(friends: List.generate(8, (_) => Person.random())));
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,29 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_study_skeleton_app/src/person/person.dart';
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key, required this.friends});
+class MainApp extends StatefulWidget {
+  MainApp({super.key, required this.friends});
 
+  final TextEditingController controller = TextEditingController();
   final List<Person> friends;
 
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text("Friend list")),
-        body: ListView.builder(
-          key: const Key('people_list'),
-          itemCount: friends.length,
-          itemBuilder: (context, index) {
-            var friend = friends[index];
+        body: Expanded(
+          child: ListView.builder(
+            key: const Key('people_list'),
+            itemCount: widget.friends.length,
+            itemBuilder: (context, index) {
+              var friend = widget.friends[index];
 
-            return ListTile(
-              leading: const Icon(Icons.person),
-              title: Text(
+              return Dismissible(
                 key: Key(friend.id),
-                friend.name,
+                direction: DismissDirection.endToStart,
+                background: Container(
+                  color: Colors.red,
+                  alignment: Alignment.centerRight,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: const Icon(Icons.delete, color: Colors.white),
+                ),
+                child: ListTile(
+                  leading: const Icon(Icons.person),
+                  title: Text(
+                    friend.name,
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        bottomSheet: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text('Friend''s name:'),
+              TextField(
+                controller: widget.controller,
               ),
-            );
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            var controller = widget.controller;
+            var input = controller.text;
+            if (input.isNotEmpty) {
+              setState(() {
+                widget.friends.add(Person(input));
+                controller.clear();
+              });
+            }
           },
         ),
       ),
